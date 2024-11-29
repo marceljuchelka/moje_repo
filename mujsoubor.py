8b4afce9052ea5a2c865a1af3ae68c94e472bc6c
Marcel Juchelka <xer@seznam.cz>
2024-11-29 08:03:37 +0100

    print a while

--- mujsoubor.py
+++ mujsoubor.py
@@ -1,2 +1,19 @@
-print("Ahoj, Python ve VS Code!")
-print("3 radek")
+aaa = 15
+bbb = 10
+
+print("aaa = \t", aaa, "\tbbb = \t", bbb)
+
+if aaa < bbb:
+    print(" aaa je mensi nez bbb")
+else:
+    print("bbb je mensi nez aaa")
+    
+i = 1
+while i <= aaa:
+    print(i)
+    i += 1  # Zvýšíme hodnotu i o 1
+    
+odpoved = ""
+while odpoved != "ano":
+    odpoved = input("zadejte 'ano' pro pokracovani")
+print("pokracujeme")
